bf6eafc946c0ff1d7ae4217c372c6f000f9d63a9
vaeyo <32903680+vaeyo@users.noreply.github.com>
2017-11-01 12:32:30 -0600

    Update initial.setup.sh

--- initial.setup.sh
+++ initial.setup.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
-#check for root
-if [[ $EUID -ne 0 ]]; then
-   else
-   echo "This script should not be run as root"
-   echo "Please restart the script from your non-root user"
-fi
+read -p "Name of User to to run docker containers under: " uname
 
-# Testing for Internet Connection
-if ping -q -c 1 -W 1 google.com >/dev/null; then
-  echo "The network is up"
-else
-  echo "The network is down"
-  echo "Please restart when service returns"
-fi
-
-uid=$(id -u)
-gid=$(id -g)
+uid=$(id -u $uname)
+gid=$(id -g $uname)
 tz="$(cat /etc/timezone)"
 
+# Letsencrypt Cert Email
 read -p "Enter email youd like associated with this server then [Enter]: " emails
 
+# Domain we want certs for
 echo "The next question is about your domain "
 echo "Please enter the root address of the domain you own"
 echo "i.e., example.com, mysite.net. Avoid the www http://"
@@ -30,16 +19,12 @@ read -p "Okay now lets enter the domain and press [Enter]: " domain
 ----------------
 # UID
 sudo find . -type f -exec sed -i "s/-uid-/"$uid"/g" '{}' \;
-
 # GID
 sudo find . -type f -exec sed -i "s/-gid-/"$gid"/g" '{}' \;
-
 # TZ
 sudo find . -type f -exec sed -i "s/-tz-/"$tz"/g" '{}' \;
-
 # Email
 sudo find . -type f -exec sed -i "s/-email-/"$emails"/g" '{}' \;
-
 # domain
 sudo find . -type f -exec sed -i "s/-domain-/"$domain"/g" '{}' \;
 

8b5e7e8f2c9493a5ce637e329bf7c32be9eb69a8
vaeyo <32903680+vaeyo@users.noreply.github.com>
2017-11-01 19:48:34 -0600

    Update initial.setup.sh

--- initial.setup.sh
+++ initial.setup.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
-read -p "Name of User to to run docker containers under: " uname
+read -p "Your username? This if for ownership and permissions of containers: " uname
+read -p "Enter email address for SSL cerificates and Press [Enter]: " emails
 
 uid=$(id -u $uname)
 gid=$(id -g $uname)
 tz="$(cat /etc/timezone)"
 
-# Letsencrypt Cert Email
-read -p "Enter email youd like associated with this server then [Enter]: " emails
-
-# Domain we want certs for
-echo "The next question is about your domain "
-echo "Please enter the root address of the domain you own"
-echo "i.e., example.com, mysite.net. Avoid the www http://"
-read -p "Okay now lets enter the domain and press [Enter]: " domain
+echo "We need your domain root for the SSL certs" 
+echo "i.e example.com or mysite.com, avoid www.example.com or https:// etc.."
+read -p "Enter the domain youd like to secure and press [Enter]: " domain
 
 # Make the edits
 ----------------
 # UID
-sudo find . -type f -exec sed -i "s/-uid-/"$uid"/g" '{}' \;
+sudo find . -type f -name "*.env" -exec sed -i "s/-uid-/"$uid"/g" '{}' \;
 # GID
-sudo find . -type f -exec sed -i "s/-gid-/"$gid"/g" '{}' \;
+sudo find . -type f -name "*.env" -exec sed -i "s/-gid-/"$gid"/g" '{}' \;
 # TZ
-sudo find . -type f -exec sed -i "s/-tz-/"$tz"/g" '{}' \;
+sudo find . -type f -name "*.env" -exec sed -i "s/-tz-/"$tz"/g" '{}' \;
 # Email
 sudo find . -type f -exec sed -i "s/-email-/"$emails"/g" '{}' \;
 # domain
